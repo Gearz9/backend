ee607c18387311305c48d5889d38944b33390250
shreyas-2046 <shreyasjagdale2046@gmail.com>
2023-12-03 17:19:35 +0530

    s

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,5 +1,5 @@
-const User = require('../models/AgencySchema');
-const OTP  = require('../models/OTP');
+const agency = require('../models/AgencySchema');
+const OTP  = require('../models/OTPSchema');
 const otpGenerator = require('otp-generator');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
@@ -13,12 +13,12 @@ exports.sendOTP = async(req,res) => {
         const {email} = req.body;
         console.log(email);
     //check if user already exist
-    const checkUserPresent = await User.findOne({email});
+    const checkAgencyPresent = await agency.findOne({email});
 
-    if(checkUserPresent){
+    if(checkAgencyPresent){
         return res.status(401).json({
             success:false,
-            message:'User Already Registered',
+            message:'Agency Already Registered',
         })
     }
 
@@ -62,3 +62,90 @@ exports.sendOTP = async(req,res) => {
     }   
     
 }
+
+exports.register = async(req,res) => {
+    try{ //data fetch from request body 
+     // validate kro
+     
+     const {
+         Name,
+         email,
+         password,
+         confirmPassword,
+         agencyType,
+         contactNumber,
+         otp,
+     } = req.body;
+     if(!Name ||!email ||!password ||!confirmPassword ||!otp){
+         return res.status(403).json({
+             success:false,
+             message:'All fields Are Required',
+             
+         })
+     }
+     //password match
+     if(password != confirmPassword){
+         return res.status(200).json({
+             success:false,
+             message:'Password and Confirm Password are not same',
+         })
+     }
+ 
+     //check if agency already exist
+     const existingagency = await agency.findOne({email});
+ 
+     if(existingagency){
+         return res.status(200).json({
+             success:false,
+             message:'agency is Already registerd',
+         })
+     }
+ 
+ 
+     //find most recent OTP stored for the agency
+     const recentOtp = await OTP.find({email}).sort({createdAt:-1}).limit(1);
+     if(recentOtp.length === 0){
+         return res.status(400).json({
+             success:false,
+             message:'Otp Not found',
+         })
+     }
+     else if(otp != recentOtp[0].otp){
+         return res.status(400).json({
+             success:false,
+             message:'Otp doesnt match',
+         })
+     }
+     //hash password 
+     const hashedPassword = await bcrypt.hash(password,10);
+     //store password
+     
+ 
+     const agency = await agency.create({
+         name,
+         email,
+         password: hashedPassword,
+         accountType: accountType,
+     });
+ 
+     return res.status(200).json({
+         success:true,
+         message:'agency created successfuly',
+         user
+     })
+ 
+ 
+ }
+     
+ 
+     
+ 
+     catch(err){
+         return res.status(500).json({
+             success:false,
+             message:'user cannot be registered',
+             error:err.message
+         })
+     }
+ 
+ }
--- routes/User.js
+++ routes/User.js
@@ -2,8 +2,9 @@ const express = require("express")
 const router = express.Router()
 
 
-const { sendOTP } = require('../controllers/Auth');
+const { sendOTP,register } = require('../controllers/Auth');
 
 router.post("/sendOtp", sendOTP);
+router.post("/register", register);
 
 module.exports = router;
