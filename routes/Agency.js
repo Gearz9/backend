84339e3f5322e5328d51952783f19a8f402620b5
Tushar Maurya <tusharmaurya43@gmail.com>
2023-12-19 12:00:47 +0530

    added resource

--- routes/Agency.js
+++ routes/Agency.js
@@ -5,15 +5,18 @@ const router = express.Router()
 const { sendOTP,register,login,verify } = require('../controllers/Auth');
 const { getNearby } = require('../controllers/Nearby');
 // const {sendRequestToAgency} = require('../controllers/RequestAgency');
-const {addResource} = require('../controllers/ResourceAdd');
+const {addResource} = require('../controllers/AddResource');
 const {getAllAgencies} = require('../controllers/testing');
+const { receiverAction, receiverPendingRequests } = require("../controllers/ReceiverAction");
 
 router.post("/sendOtp", sendOTP);
 router.post("/register", register);
 router.post("/login", login);
 router.get("/get-nearby", getNearby);
 router.post('/verifyOtp', verify);
-router.post('/send-request', sendRequestToAgency);
+// router.post('/send-request', sendRequestToAgency);
+router.post('/receiver-pending-requests', receiverPendingRequests)
+router.post('/receiver-action', receiverAction)
 router.post('/add-resource', addResource);
 router.get('/testing/getAllAgencies',getAllAgencies);
 
